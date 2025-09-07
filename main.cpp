b10b23ecc69a01d6daa148c3c41cb37290b8bcc7
4rtikk <nicolasvera@edu.univali.br>
2025-09-07 16:34:10 -0300

    maiknn
    a

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <cstdint> //biblioteca que contém o uint
 #include <fstream>
-//#include "intruction_types_RISCV.hpp"
+#include "intruction_types_RISCV.hpp"
+
+using namespace std;
 
 //tentando criar um programa que recebe um arquivo, lê linha por linha,
 // aplica a mascara de bits e descobre qual tipo de opcode ele é 
@@ -12,70 +14,38 @@
 
 //conflito estrutural é questão de prova
 
-
-//passa pra biblioteca
-uint32_t isolate_mask(uint32_t current_hexa, uint32_t itembit ){
-    uint32_t result = current_hexa & itembit;
-    return result;
-}
-
-//passa pra biblioteca
-std::string opcode_identifier(uint32_t opcode){
-    switch(opcode){
-        //tipo R 
-        case 0x33:
-            return "Tipo R";
-        //tipo I
-        case 0x67:
-        case 0x03:
-        case 0x13:
-        case 0x0F:
-        case 0x73:
-            return "Tipo I";
-        //tipo S
-        case 0x23:
-            return "Tipo S";
-        // tipo B
-        case 0x63:
-            return "Tipo B";
-        // tipo U
-        case 0x37:
-        case 0x17:
-            return "Tipo U";
-        // tipo J
-        case 0x6F:
-            return "Tipo J";
-        default:
-            return "Tipo desconhecido";
-    }
-}
-
-
-
-
 int main(){
     int cont_line = 0;
     uint32_t opcodebit = 0x0000007F; //cara que eu uso pra isolar os 7 primeiros bits (opcode)
     uint32_t opcode; //cara que vai receber os opcodes pra mim
-    std::string instype;
+    string instype;
 
-    std::ifstream meu_arquivo;
+    ifstream meu_arquivo;
     meu_arquivo.open("hexa_dump.txt");
 
-    std::string current_line;
+    map<string, int> contador;
+
+    string current_line;
     uint32_t current_hexa;
 
-    while(std::getline(meu_arquivo, current_line)){
+    while(getline(meu_arquivo, current_line)){
+      if(current_line.empty()) continue; 
         cont_line++;
 
-        current_hexa = std::stoul(current_line, nullptr, 16); //converte pra hexadecimal a string da linha
+        current_hexa = stoul(current_line, nullptr, 16); //converte pra hexadecimal a string da linha
 
         opcode = isolate_mask(current_hexa, opcodebit);
         instype = opcode_identifier(opcode);
 
+        contador[instype]++;
 
-
-        std:: cout << "\nLinha " << cont_line << " instrucao '" << current_line << "' eh: " << instype;
+        cout << "\nLinha " << cont_line << " instrucao '" << current_line << "' eh: " << instype;
     }
+
+    cout << "\n\n===== RESUMO =====\n";
+    for(auto &par : contador){
+        cout << par.first << " -> " << par.second << " instrucoes\n";
+    }
+
     
 }
